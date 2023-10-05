2ab3010afe31fe8f63fb30b2f140a4f9a43d20c7
Gabriel Rodrigues <gabriel.rodrigues@eficazmarketing.com>
2023-10-05 17:51:35 -0300

    feat: add new view template

---
+++ resources/views/layouts/template.blade.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>@yield('title')</title>
+
+    <!-- Google Fonts -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Inter:wght@300;400;500;700;800;900&display=swap"
+        rel="stylesheet">
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="{{ asset('assets/css/reset.css') }}" rel="preload">
+    <link rel="stylesheet" href="{{ asset('assets/css/default.css') }}" rel="preload">
+    @yield('styles')
+
+    <!-- Scripts -->
+    <script id="defaultScript" type="text/javascript" src="{{ asset('assets/js/index.js') }}"></script>
+</head>
+
+<body>
+    @yield('navbar')
+
+    <div class="main">
+        @yield('content')
+    </div>
+</body>
+
+</html>
